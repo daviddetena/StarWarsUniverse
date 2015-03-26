616e516991b9c743376df8e78ecedbd0b8bd6a7d
David de Tena <daviddetenacalvo@gmail.com>
2015-03-26 13:12:53 +0100

    second version of UITabBarVC with 5 VC with push view controllers to wiki

--- StarWars/AppDelegate.m
+++ StarWars/AppDelegate.m
@@ -22,24 +22,12 @@
     self.window = [[UIWindow alloc]
                    initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    // Create the model
-    NSURL *vaderURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Darth_Vader"];
-    NSBundle *b = [NSBundle mainBundle];
-    NSData *vaderSound = [NSData dataWithContentsOfURL:[b URLForResource:@"vader" withExtension:@"caf"]];
-    UIImage *vaderImage = [UIImage imageNamed:@"darthVader.jpg"];
-    DTCStarWarsCharacter *model = [[DTCStarWarsCharacter alloc]initWithName:@"Anakin Skywalker" alias:@"Darth Vader" url:vaderURL soundData:vaderSound photo:vaderImage];
-    
-    // Create the character and wiki controllers
-    DTCCharacterViewController *characterVC = [[DTCCharacterViewController alloc]initWithModel:model];
-    //DTCWikiViewController *wikiVC = [[DTCWikiViewController alloc]initWithModel:model];
-    
-    
-    // Creo el combinador
-    UINavigationController *navVC = [[UINavigationController alloc]initWithRootViewController:characterVC];
+    // Tab bar for grouping all the characters
+    UITabBarController *tabBar= [[UITabBarController alloc]init];
+    tabBar.viewControllers = [self arrayOfControllers];
     
     // Set controller as the root VC
-    self.window.rootViewController = navVC;
-    
+    self.window.rootViewController = tabBar;
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor orangeColor];
@@ -67,6 +55,56 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Utils
+- (NSArray *) arrayOfModels{
+    NSBundle *b = [NSBundle mainBundle];
+    
+    // Create the models and their VC
+    NSURL *vaderURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Darth_Vader"];
+    NSData *vaderSound = [NSData dataWithContentsOfURL:[b URLForResource:@"vader" withExtension:@"caf"]];
+    UIImage *vaderImage = [UIImage imageNamed:@"darthVader.jpg"];
+    DTCStarWarsCharacter *vader = [[DTCStarWarsCharacter alloc]initWithName:@"Anakin Skywalker" alias:@"Darth Vader" url:vaderURL soundData:vaderSound photo:vaderImage];
+    
+    // Chewbacca
+    NSURL *chewieURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Chewbacca"];
+    NSData *chewieSound = [NSData dataWithContentsOfURL:[b URLForResource:@"chewbacca" withExtension:@"caf"]];
+    UIImage *chewieImage = [UIImage imageNamed:@"chewbacca.jpg"];
+    DTCStarWarsCharacter *chewie = [[DTCStarWarsCharacter alloc]initWithAlias:@"Chewbacca" url:chewieURL soundData:chewieSound photo:chewieImage];
+    
+    // C-3PO
+    NSURL *c3poURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/C-3PO"];
+    NSData *c3poSound = [NSData dataWithContentsOfURL:[b URLForResource:@"c3po" withExtension:@"caf"]];
+    UIImage *c3poImage = [UIImage imageNamed:@"c3po.jpg"];
+    DTCStarWarsCharacter *c3po = [[DTCStarWarsCharacter alloc]initWithAlias:@"C-3PO" url:c3poURL soundData:c3poSound photo:c3poImage];
+    
+    // Yoda
+    NSURL *yodaURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Yoda"];
+    NSData *yodaSound = [NSData dataWithContentsOfURL:[b URLForResource:@"yoda" withExtension:@"caf"]];
+    UIImage *yodaImage = [UIImage imageNamed:@"yoda.jpg"];
+    DTCStarWarsCharacter *yoda = [[DTCStarWarsCharacter alloc]initWithName:@"Minch Yoda" alias:@"Master Yoda" url:yodaURL soundData:yodaSound photo:yodaImage];
+    
+    // R2-D2
+    NSURL *r2d2URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/R2-D2"];
+    NSData *r2d2Sound = [NSData dataWithContentsOfURL:[b URLForResource:@"r2-d2" withExtension:@"caf"]];
+    UIImage *r2d2Image = [UIImage imageNamed:@"R2-D2.jpg"];
+    DTCStarWarsCharacter *r2d2 = [[DTCStarWarsCharacter alloc]initWithAlias:@"R2-D2" url:r2d2URL soundData:r2d2Sound photo:r2d2Image];
+    
+    return @[vader,chewie,c3po,yoda,r2d2];
+}
+
+- (NSArray *) arrayOfControllers{
+    NSArray *models = [self arrayOfModels];
+    NSMutableArray *controllers = [NSMutableArray arrayWithCapacity:[models count]];
+
+    // One DTCCharacterVC and one NavigationController per model
+    for (DTCStarWarsCharacter *model in models) {
+        DTCCharacterViewController *characterVC = [[DTCCharacterViewController alloc]initWithModel:model];
+        UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:characterVC];
+        [controllers addObject:navController];
+    }
+    return controllers;
 }
 
 @end
